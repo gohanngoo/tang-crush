fbcd7f66776d3b0b9de0ca5cf7450f233afefe80
gohanngoo <116343675+gohanngoo@users.noreply.github.com>
2022-10-21 15:54:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Châu à!',
+    introDesc: `
+    Có người nói Anh không phải Lạc Long Quân
+    Nhưng Anh vẫn thích Chờ..Âu cơ.`,
+    btnIntro: 'Hihi',
+    title: 'Phải chăng Anh đã thích Em ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Cho anh làm quen bé nhóa <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Em lại chọn Anh <3',
+    btnReply: 'Gửi cho Anh <3',
+    reply: 'Quen thì yêu mà không yêu thì yêu <33333333',
+    mess: '2,3 con mực 🦑. Thích Bé nhiều cực 😘😘',
+    messDesc: 'Tối nay 7h, Anh qua đón Bé nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
